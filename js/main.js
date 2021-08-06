3f6b66adedd84937202a6923506f260e2a9611da
Tom Porter <tom.port@yahoo.co.uk>
2021-08-06 14:30:13 +0100

    img const

--- js/main.js
+++ js/main.js
@@ -4,4 +4,7 @@ window.addEventListener("load", () => {
   console.log("where to begin?")
 })
 
-const images = document.querySelectorAll(".carousel");
+const images = document.querySelectorAll(".carousel"); // all images for carousel in variable
+
+
+
