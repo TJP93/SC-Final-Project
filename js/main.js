92070048bb8d7b448572a068a458614e038b5861
Tom Porter <tom.port@yahoo.co.uk>
2021-08-06 15:32:49 +0100

    written automatic carousel with 5sec interval

--- js/main.js
+++ js/main.js
@@ -4,7 +4,26 @@ window.addEventListener("load", () => {
   console.log("where to begin?")
 })
 
-const images = document.querySelectorAll(".carousel"); // all images for carousel in variable
+const images = document.querySelectorAll(".carousel"); // all images for carousel in variable -- nodelist object to be iterated through by loop
 
 
+let imageNumber = 0; // variable to manipulate images shown
 
+
+function myCarousel() {
+
+  for (let i = 0; i < images.length; i++) {
+    
+    images[i].style.display = "none";}
+
+  imageNumber++;
+
+  if (imageNumber > images.length) {imageNumber = 1}
+
+  images[imageNumber-1].style.display = "block";
+
+  
+}
+setInterval(myCarousel, 5000); // set to change image every 5 sec
+
+myCarousel(); // call
